801dde5467bd90f7a695bf3a856382da9d2417dd
Declan Brady <declan.brady@scale.com>
2026-03-30 10:39:35 -0400

    Enable Task status updates via POST requests (#174)

--- agentex-ui/lib/types.ts
+++ agentex-ui/lib/types.ts
@@ -1,3 +1,5 @@
+import { Task } from 'agentex/resources';
+
 export type JsonValue =
   | string
   | number
@@ -5,3 +7,26 @@ export type JsonValue =
   | null
   | JsonValue[]
   | { [key: string]: JsonValue };
+
+export type TaskStatus = NonNullable<Task['status']>;
+
+export enum TaskStatusEnum {
+  CANCELED = 'CANCELED',
+  COMPLETED = 'COMPLETED',
+  FAILED = 'FAILED',
+  RUNNING = 'RUNNING',
+  TERMINATED = 'TERMINATED',
+  TIMED_OUT = 'TIMED_OUT',
+  DELETED = 'DELETED',
+}
+
+// API to frontend type checks
+export const _taskStatusEnumCheck: Record<TaskStatus, true> = {
+  [TaskStatusEnum.CANCELED]: true,
+  [TaskStatusEnum.COMPLETED]: true,
+  [TaskStatusEnum.FAILED]: true,
+  [TaskStatusEnum.RUNNING]: true,
+  [TaskStatusEnum.TERMINATED]: true,
+  [TaskStatusEnum.TIMED_OUT]: true,
+  [TaskStatusEnum.DELETED]: true,
+};
